f5397c2ec3da558b7b3743a7689e56a08c971f78
tdp93422 <tim.v2.0@gmail.com>
2013-09-30 15:12:52 -0700

    cleaning up remenants from scaffold

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -7,9 +7,6 @@ import Yesod.Auth
 getHomeR :: Handler Html
 getHomeR = do
     mAuth <- maybeAuth
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
     defaultLayout $ do
         aDomId <- newIdent
         setTitle "Welcome To WriteMeAList.com!"
@@ -19,19 +16,5 @@ getHomeR = do
                    content="hTMPzastDoFcR4-DLMM2jHogIhG5oWDRAZ4L-BVTWiU">
             |]
 
-        $(widgetFile "homepage")
-
-postHomeR :: Handler Html
-postHomeR = do
-    mAuth <- maybeAuth
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
-
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To WriteMeAList.com!"
         $(widgetFile "homepage")
 
