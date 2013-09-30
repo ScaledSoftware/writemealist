e55687872aba4f6fd97feb1d7ae83e99d12642c5
tdp93422 <tim.v2.0@gmail.com>
2013-09-30 14:48:16 -0700

    change welcome message -- embarrasing oversite to not welcome users to WriteMeAList.com

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,13 +4,6 @@ module Handler.Home where
 import Import
 import Yesod.Auth
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
     mAuth <- maybeAuth
@@ -19,10 +12,11 @@ getHomeR = do
         handlerName = "getHomeR" :: Text
     defaultLayout $ do
         aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
+        setTitle "Welcome To WriteMeAList.com!"
         toWidgetHead
             [hamlet|
-                <meta name="google-site-verification" content="hTMPzastDoFcR4-DLMM2jHogIhG5oWDRAZ4L-BVTWiU">
+                <meta name="google-site-verification"
+                   content="hTMPzastDoFcR4-DLMM2jHogIhG5oWDRAZ4L-BVTWiU">
             |]
 
         $(widgetFile "homepage")
@@ -38,10 +32,6 @@ postHomeR = do
 
     defaultLayout $ do
         aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
+        setTitle "Welcome To WriteMeAList.com!"
         $(widgetFile "homepage")
 
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderDivs $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField "What's on the file?" Nothing
