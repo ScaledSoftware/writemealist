863a710f925c0bab262095102c7a7c6f173d0d1f
tdp93422 <tim.v2.0@gmail.com>
2013-11-04 14:24:03 -0800

    fixing categories -- can now add item to each category

--- Handler/Lists.hs
+++ Handler/Lists.hs
@@ -5,6 +5,7 @@ import Yesod.Auth
 import Data.Maybe
 import System.Random (getStdRandom, randomR)
 import Data.List (sortBy)
+import qualified Data.Text as T
 
 getSharedListShow :: ListId -> Int -> Handler Html
 getSharedListShow listId randKey = do
@@ -20,18 +21,19 @@ getSharedListShow listId randKey = do
         case mAuth of
          Nothing -> do
             aDomId <- newIdent
-            categories <- runDB $ selectList [CategoryListId ==. listId] []
-            (widget, enctype) <- generateFormPost (\x -> listItemCreateForm x categories aDomId "" Nothing)
+            catId2NameLookup <- runDB $ selectList [CategoryListId ==. listId] [Asc CategoryName] >>= (return . mapCatIdToName)
             (catwidget, catenctype) <- generateFormPost listCategoryCreateForm
+
             list <- runDB $ get404 listId
             listItems <- runDB $ selectList [ListItemList ==. listId]
                                             [Asc ListItemName, Asc ListItemCreatedAt]
-            let itemsByCategory = groupItemsByCategory categories listItems
+
+            let itemsByCategory = groupItemsByCategory catId2NameLookup listItems
+            itemsByCatWAdd <- mapM (\(catName, lis) -> generateFormPost (\x -> listItemCreateKnownCatForm x "" (Just catName)) >>= (\formRes -> return (catName, lis, formRes))) itemsByCategory
 
             defaultLayout $ do
                 setTitleI $ MsgListTitle (listName list)
                 $(widgetFile "topNav")
-                $(widgetFile "listItemCreate")
                 $(widgetFile "list")
          Just (Entity userId _) -> do
              maybeListEditor <- runDB $ getBy $ UniqueListEditor listId userId
@@ -89,49 +91,51 @@ getListR listId = do
                                         [Asc ListItemName, 
                                          Asc ListItemCreatedAt]
 
-        categories <- runDB $ selectList [CategoryListId ==. listId] []
-        let itemsByCategory = groupItemsByCategory categories listItems
+        catId2NameLookup <- runDB $ selectList [CategoryListId ==. listId] [] >>= (return . mapCatIdToName)
+        let itemsByCategory = groupItemsByCategory catId2NameLookup listItems
+        itemsByCatWAdd <- mapM (\(catName, lis) -> generateFormPost (\x -> listItemCreateKnownCatForm x "" (Just catName)) >>= (\formRes -> return (catName, lis, formRes))) itemsByCategory
 
         let createItemAllowed = True
         aDomId <- newIdent
-        (widget, enctype) <- generateFormPost (\x -> listItemCreateForm x categories aDomId "" Nothing)
+        (widget, enctype) <- generateFormPost (\x -> listItemCreateForm x catId2NameLookup aDomId "" Nothing)
         (catwidget, catenctype) <- generateFormPost listCategoryCreateForm
         defaultLayout $ do
             setTitleI $ MsgListTitle (listName list)
             $(widgetFile "topNav")
             $(widgetFile "list")
 
-catIdToNameLookup :: [Entity Category] -> [(CategoryId, Text)]
-catIdToNameLookup cats = zip (map entityKey cats) (map (categoryName . entityVal) cats)
+mapCatIdToName :: [Entity Category] -> [(CategoryId, Text)]
+mapCatIdToName cats = zip (map entityKey cats) (map (categoryName . entityVal) cats)
 
-catIdToName :: [Entity Category] -> Maybe CategoryId -> Maybe Text
-catIdToName _  Nothing = Nothing
-catIdToName [] _ = Nothing
-catIdToName ((Entity catId cat):cats) (Just cId) =
-    if catId == cId
-        then Just $ categoryName cat
-        else catIdToName cats (Just cId)
+mapCatNameToId :: [Entity Category] -> [(Text, CategoryId)]
+mapCatNameToId cats = zip (map (categoryName . entityVal) cats) (map entityKey cats)
 
-catNameToId :: [Entity Category] -> Maybe Text -> Maybe CategoryId
-catNameToId _          Nothing = Nothing
-catNameToId []         _       = Nothing
-catNameToId ((Entity key cat):categories) (Just catName) = 
-    if (categoryName cat) == catName
-        then Just key
-        else catNameToId categories (Just catName)
+catNameToId :: [(Text, CategoryId)] -> Maybe Text -> Maybe CategoryId
+catNameToId eCats mName = mName >>= flip lookup eCats
 
 listItemCategoryName :: [(CategoryId, Text)] -> ListItem -> Text
-listItemCategoryName cat2Name li =
-        if isNothing lookupId 
-            then ""
-            else fromMaybe "" $ (flip lookup) cat2Name (fromJust lookupId)
+listItemCategoryName catToName li = fromMaybe "" (catIdToName catToName (listItemCatId li))
     where
-        lookupId = listItemCatId li
+        catIdToName :: [(CategoryId, Text)] -> Maybe CategoryId -> Maybe Text
+        catIdToName eCats mCatId = mCatId >>= flip lookup eCats
 
-groupItemsByCategory :: [Entity Category] -> [Entity ListItem] -> [(Text, [Entity ListItem])]
-groupItemsByCategory cats itemList = groupItemsByCategory' catItem
+        
+
+--groupItemsByCategory :: [(CategoryId, Text)] -> [Entity ListItem] -> [(Text, [Entity ListItem])]
+--groupItemsByCategory catIdNames itemList = groupItemsByCategory' catItem
+--  where
+--    catItem = sortBy (\(c1, _) (c2, _) -> compare c1 c2) $ 
+--                map (\li -> (listItemCategoryName catIdNames $ entityVal li, li)) itemList
+--
+--    groupItemsByCategory' :: [(Text, Entity ListItem)] -> [(Text, [Entity ListItem])]
+--    groupItemsByCategory' [] = []
+--    groupItemsByCategory' list@((catName,_):_) =
+--                (catName, map snd thisCatList) : groupItemsByCategory' otherCatsList
+--      where
+--        (thisCatList, otherCatsList) = span (\(name, _) -> name == catName) list
+groupItemsByCategory :: [(CategoryId, Text)] -> [Entity ListItem] -> [(Text, [Entity ListItem])]
+groupItemsByCategory catIdNames itemList = groupItemsByCategory' catItem
   where
-    catIdNames = catIdToNameLookup cats
     catItem = sortBy (\(c1, _) (c2, _) -> compare c1 c2) $ 
                 map (\li -> (listItemCategoryName catIdNames $ entityVal li, li)) itemList
 
@@ -149,11 +153,13 @@ postListItemCreateR listId = do
     let mAuth = Just uId
     aDomId <- newIdent
     categories <- runDB $ selectList [CategoryListId ==. listId] []
-    ((res, widget), enctype) <- runFormPost (\x -> listItemCreateForm x categories aDomId "" Nothing)
+    let catId2NameLookup = mapCatIdToName categories
+    ((res, widget), enctype) <- runFormPost (\x -> listItemCreateForm x catId2NameLookup aDomId "" Nothing)
 
     case res of 
         FormSuccess (listItemText, mCatName) -> do
-            let mCatId = catNameToId categories mCatName
+            (liftIO . print) mCatName
+            let mCatId = catNameToId (mapCatNameToId categories) mCatName
             currTime <- lift getCurrentTime
             _ <- runDB $ insert (ListItem listItemText listId uId currTime Nothing Nothing mCatId)
             redirect $ ListR listId
@@ -168,7 +174,7 @@ postListItemCreateR listId = do
 getListItemCreateR :: ListId -> Handler Html
 getListItemCreateR listId = do
     aDomId <- newIdent
-    categories <- runDB $ selectList [CategoryListId ==. listId] []
+    categories <- runDB $ selectList [CategoryListId ==. listId] [] >>= (return .  mapCatIdToName)
     (widget, enctype) <- generateFormPost (\x -> listItemCreateForm x categories aDomId "" Nothing)
     list <- runDB $ get404 listId
     defaultLayout $ do
@@ -181,12 +187,14 @@ postListItemEditR listId listItemId = do
     Entity uId _ <- requireAuth
     aDomId <- newIdent
     categories <- runDB $ selectList [CategoryListId ==. listId] []
-    ((res, widget), enctype) <- runFormPost (\x -> listItemCreateForm x categories aDomId "" Nothing)
+    let catIdToNames = mapCatIdToName categories
+    ((res, widget), enctype) <- runFormPost (\x -> listItemCreateForm x catIdToNames aDomId "" Nothing)
 
     case res of 
         FormSuccess (itemName, mCatName) -> do
+            let lookupIdFromName = mapCatNameToId categories
             _ <- runDB $ update listItemId [ListItemName =. itemName,
-                                            ListItemCatId =. (catNameToId categories mCatName)] 
+                                            ListItemCatId =. (catNameToId lookupIdFromName mCatName)] 
             redirect $ ListR listId
         _ -> do 
             list <- runDB $ get404 listId
@@ -200,14 +208,15 @@ postListItemEditR listId listItemId = do
 getListItemEditR :: ListId -> ListItemId -> Handler Html
 getListItemEditR listId listItemId = do
     listItem <- runDB $ get404 listItemId
-
     let itemName = listItemName listItem
+
     categories <- runDB $ selectList [CategoryListId ==. listId] []
-    let category = (catIdToName categories) $ listItemCatId listItem
+    let catIdToNames = mapCatIdToName categories
+    let catName = Just $ listItemCategoryName catIdToNames listItem
 
     aDomId <- newIdent
 
-    (widget, enctype) <- generateFormPost (\x -> listItemCreateForm x categories aDomId itemName category)
+    (widget, enctype) <- generateFormPost (\x -> listItemCreateForm x catIdToNames aDomId itemName catName)
     list <- runDB $ get404 listId
     mAuth <- maybeAuth
 
@@ -239,11 +248,10 @@ postListItemCompleteR listId listItemId = do
 
     redirect $ ListR listId
 
-listItemCreateForm :: Html -> [Entity Category] -> Text -> Text -> Maybe Text
+listItemCreateForm :: Html -> [(CategoryId, Text)] -> Text -> Text -> Maybe Text
                            -> MForm Handler (FormResult (Text, Maybe Text), Widget)
-listItemCreateForm extra categories aDomId entryName catName = do
-    let catNames = map (categoryName . entityVal) categories
-    let cats = zip catNames catNames
+listItemCreateForm extra catIdToNames aDomId entryName catName = do
+    let cats = (\x -> zip x x) $ map snd catIdToNames 
     (itemRes, itemView) <- mreq textField ((fieldSettingsLabel MsgListItemCreateLabel) {fsId = Just aDomId}) (Just entryName)
     (catRes, catView) <- mopt (selectFieldList cats) (fieldSettingsLabel MsgListItemCategoryLabel) (Just catName)
 
@@ -256,6 +264,25 @@ listItemCreateForm extra categories aDomId entryName catName = do
               |]
         [whamlet|
                     #{extra}^{fvInput itemView} &nbsp ^{fvInput catView}
+        |]
+
+    return (result, widget)
+
+listItemCreateKnownCatForm :: Html -> Text -> Maybe Text
+                           -> MForm Handler (FormResult (Text, Maybe Text), Widget)
+listItemCreateKnownCatForm extra entryName catName = do
+    (itemRes, itemView) <- mreq textField "" (Just entryName)
+    (catRes, catView) <- mopt hiddenField "" (Just catName)
+    (liftIO . putStrLn) ("CatName when creating form was: " ++ (show catName))
+
+    let result = (,) <$> itemRes <*> catRes
+    let widget = do
+        toWidget 
+              [lucius|
+                    ##{fvId itemView} { width: 8em; }
+              |]
+        [whamlet|
+                    #{extra}^{fvInput itemView}^{fvInput catView}
         |]
 
     return (result, widget)
@@ -317,3 +344,32 @@ postListCategoryCreateR listId = do
         _ -> do
             -- oh well
             redirect $ ListR listId
+
+fixNoCategoryName :: Text -> Text
+fixNoCategoryName t = if T.null t
+                        then "Un-categorized items"
+                        else t
+
+postListItemCreateKnownCatR :: ListId -> Handler Html
+postListItemCreateKnownCatR listId = do
+    -- TODO: Add check that this user owns this list
+    Entity uId _ <- requireAuth
+    let mAuth = Just uId
+    aDomId <- newIdent
+    categories <- runDB $ selectList [CategoryListId ==. listId] []
+    let catId2NameLookup = mapCatIdToName categories
+    ((res, widget), enctype) <- runFormPost (\x -> listItemCreateKnownCatForm x "" Nothing)
+
+    case res of 
+        FormSuccess (listItemText, mCatName) -> do
+            (liftIO . putStrLn) ("knownCat catName: " ++ (show mCatName))
+            let mCatId = catNameToId (mapCatNameToId categories) mCatName
+            currTime <- lift getCurrentTime
+            _ <- runDB $ insert (ListItem listItemText listId uId currTime Nothing Nothing mCatId)
+            redirect $ ListR listId
+        _ -> do 
+            list <- runDB $ get404 listId
+            defaultLayout $ do
+                setTitleI MsgEnterListItemName
+                $(widgetFile "topNav")
+                $(widgetFile "listItemCreate")
